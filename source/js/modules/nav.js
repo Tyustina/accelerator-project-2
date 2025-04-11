ebf52f31fa909f73ae514be2998ddf4007faecc1
Anastasiia Tyustina <tyustina.av@yandex.ru>
2025-04-11 16:03:48 +0500

    Добавляет блок hero и слайдер

--- source/js/modules/nav.js
+++ source/js/modules/nav.js
@@ -1,3 +1,5 @@
+const bodyPage = document.querySelector('.page-body');
+const bodyOverlay = document.querySelector('.page-body__overlay');
 const navContainer = document.querySelector('.nav');
 const navButton = document.querySelector('.nav__button');
 
@@ -7,10 +9,16 @@ navButton.addEventListener('click', () =>{
     navContainer.classList.add('nav--closed');
     navButton.classList.remove('nav__button--opened');
     navButton.classList.add('nav__button--closed');
+    bodyOverlay.style.display = 'none';
+    bodyOverlay.style.opacity = '0';
+    bodyPage.style.overflow = 'visible';
   }else {
     navContainer.classList.remove('nav--closed');
     navContainer.classList.add('nav--opened');
     navButton.classList.remove('nav__button--closed');
     navButton.classList.add('nav__button--opened');
+    bodyOverlay.style.display = 'block';
+    bodyOverlay.style.opacity = '1';
+    bodyPage.style.overflow = 'hidden';
   }
 });
--- source/js/modules/slider.js
+++ source/js/modules/slider.js
@@ -1,10 +1,25 @@
 // https://swiperjs.com/get-started#installation
 import Swiper from 'swiper';
-import { Navigation, Pagination } from 'swiper/modules';
+import {Navigation,Pagination} from 'swiper/modules';
 import 'swiper/css';
 
 new Swiper('.hero__swiper', {
-  modules: [Navigation,Pagination],
+  modules: [Navigation, Pagination],
   loop: true,
   slidesPerView: 1,
+  pagination: {
+    el: '.swiper-pagination',
+    bulletClass: 'main-slider__bullet',
+    bulletActiveClass: 'main-slider__bullet--active',
+    type: 'bullets',
+    clickable: true,
+    renderBullet: function (index, className){
+      return '<button class="' + className + '" aria-label="Перейти к слайду ' + (index + 1) + '"></button>';
+    },
+  },
+  breakpoints: {
+    1440: {
+      allowTouchMove: false,
+    }
+  }
 });
