375ff834b2c48b64ea2b80dea57b4027743ac128
Anastasiia Tyustina <103989119+Tyustina@users.noreply.github.com>
2025-04-27 01:33:27 +0500

    Добавляет верстку галереи

--- source/js/modules/gallery.js
+++ source/js/modules/gallery.js
@@ -1,19 +1,26 @@
 import Swiper from 'swiper';
 import { Navigation, Mousewheel } from 'swiper/modules';
 import 'swiper/css';
+let gallerySwiper;
 
-new Swiper('.gallery__swiper', {
-  modules: [Navigation, Mousewheel],
-  loop: false,
-  navigation: {
-    nextEl: '.gallery__button--next',
-    prevEl: '.gallery__button--prev',
-  },
-  slidesPerView: 2,
-  slidesPerGroup: 1,
-  breakpoints: {
-    768: {
-      slidesPerView: 3,
+if (window.innerWidth < 1440 && !gallerySwiper) {
+
+  gallerySwiper = new Swiper('.gallery__swiper', {
+    modules: [Navigation, Mousewheel],
+    loop: true,
+    navigation: {
+      nextEl: '.gallery__button--next',
+      prevEl: '.gallery__button--prev',
+    },
+    spaceBetween: 5,
+    slidesPerView: 2,
+    slidesPerGroup: 1,
+    breakpoints: {
+      768: {
+        slidesPerView: 3,
+      }
     }
-  }
-});
+  });
+} else if (window.innerWidth < 1440 && gallerySwiper) {
+  gallerySwiper.destroy(true, true);
+}
